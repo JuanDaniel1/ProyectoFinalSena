9de9a05bc86eae330e98a0f223f6d1fbdeb6822a
Aprendizsena2023 <juandabogo@gmail.com>
2023-10-21 23:37:57 -0500

    Cargo de prueba

---
+++ lib/hola.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HolaTest extends StatefulWidget {
+  const HolaTest({super.key});
+
+  @override
+  State<HolaTest> createState() => _HolaTestState();
+}
+
+class _HolaTestState extends State<HolaTest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
