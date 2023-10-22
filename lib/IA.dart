a7bdd85e96fd6a123b409b4efcf90dccd52cfe9e
Aprendizsena2023 <juandabogo@gmail.com>
2023-10-21 22:04:07 -0500

    IA

---
+++ lib/IA.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IAnice extends StatefulWidget {
+  const IAnice({super.key});
+
+  @override
+  State<IAnice> createState() => _IAniceState();
+}
+
+class _IAniceState extends State<IAnice> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
