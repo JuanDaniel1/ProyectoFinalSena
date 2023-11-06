b89a9ea68c452efb53585cfad68292dfa1e28f58
Aprendizsena2023 <juandabogo@gmail.com>
2023-11-06 18:59:25 -0500

    ProductoCard

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -6,96 +6,113 @@ import 'package:shop_app/screens/details/details_screen.dart';
 import '../constants.dart';
 import '../size_config.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCard extends StatefulWidget {
   const ProductCard({
     Key? key,
     this.width = 60,
     this.aspectRetio = 1.02,
     required this.product,
   }) : super(key: key);
-
   final double width, aspectRetio;
   final Product product;
 
+  @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  bool isHover = false;
+  Offset mousPos = new Offset(0, 0);
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(5 ),
       child: Card(
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(18.0),
-              ),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.green,
-                  spreadRadius: 4,
-                  blurRadius: 10,
-                  offset: Offset(0,6)
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(18.0),
                 ),
-                BoxShadow(
-                  color: Colors.green,
-                  spreadRadius: -4,
-                  blurRadius: 5,
-                  offset: Offset(6,0)
-                )
-              ]),
-          child:  SizedBox(
-            child: GestureDetector(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.green,
+                      spreadRadius: 4,
+                      blurRadius: 10,
+                      offset: Offset(0,6)
+                  ),
+                  BoxShadow(
+                      color: Colors.green,
+                      spreadRadius: -4,
+                      blurRadius: 5,
+                      offset: Offset(6,0)
+                  )
+                ]),
+            child:  SizedBox(
+              child: GestureDetector(
                 onTap: () => Navigator.pushNamed(
                   context,
                   DetailsScreen.routeName,
-                  arguments: ProductDetailsArguments(product: product),
+                  arguments: ProductDetailsArguments(product: widget.product),
                 ),
                 child: SingleChildScrollView(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
                       AspectRatio(
-                        aspectRatio: 1.02,
-                        child: Container(
-                          padding: EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            color: kSecondaryColor.withOpacity(0.1),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Hero(
-                            tag: product.id.toString(),
-                            child: Image.asset(product.images[0]),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        product.title,
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                        maxLines: 2,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Precio: \$${product.price}",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w600,
-                              color: kPrimaryColor,
+                      aspectRatio: 1.02,
+                          child: Container(
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: kSecondaryColor.withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Hero(
+                              tag: widget.product.id.toString(),
+                              child: Image.asset(widget.product.images[0]),
                             ),
                           ),
-                        ],
-                      )
-                    ],
+
+
+
+
+              ),
+              const SizedBox(height: 10),
+              Text(
+                widget.product.title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                maxLines: 2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Precio: \$${widget.product.price}",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      color: kPrimaryColor,
+                    ),
                   ),
-                )
-
+                ],
+              )
+              ],
             ),
-          ),
-        ),
+          )
 
-      )
+      ),
+    ),
+    ),
+
+    )
 
     );
   }
+
 }
+
+
+
+
+
+
